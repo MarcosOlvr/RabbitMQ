b3ba895886f59ae564912c2ce087743c525a43c9
Marcos <marquinhos.dev@gmail.com>
2023-03-14 11:34:50 -0300

    finalizando projeto 👍

--- Rabbit.Consumer/Program.cs
+++ Rabbit.Consumer/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+using Rabbit.Models;
+using Rabbit.Models.Entities;
+
+var factory = new ConnectionFactory 
+{ 
+    HostName = "localhost",
+    UserName = "admin",
+    Password = "123456"
+};
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+
+    channel.QueueDeclare(queue: "rabbitMessageQueue",
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var json = Encoding.UTF8.GetString(body);
+
+        RabbitMessage message = JsonSerializer.Deserialize<RabbitMessage>(json);
+
+        Console.WriteLine($"Titulo: {message.Titulo}; Texto: {message.Texto}; Id: {message.Id};");
+    };
+    channel.BasicConsume(queue: "rabbitMessageQueue",
+                         autoAck: true,
+                         consumer: consumer);
+
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+}
---
+++ Rabbit.Repositories/RabbitMessageRepository.cs
@@ -0,0 +1,45 @@
+﻿using Rabbit.Models.Entities;
+using Rabbit.Repositories.Interfaces;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Rabbit.Repositories
+{
+    public class RabbitMessageRepository : IRabbitMessageRepository
+    {
+        public void SendMessage(RabbitMessage message)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "admin",
+                Password = "123456"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "rabbitMessageQueue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string json = JsonSerializer.Serialize(message);
+                var body = Encoding.UTF8.GetBytes(json);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "rabbitMessageQueue",
+                                     basicProperties: null,
+                                     body: body);
+
+                Console.WriteLine(" [x] Sent {0}", json);
+            }
+        }
+    }
+}
